0aa52f298136dbf1e206a35fb0111c5b586b2a3f
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-10-15 19:47:46 -0400

    style: ui product

--- src/resources/views/productos/index.blade.php
+++ src/resources/views/productos/index.blade.php
@@ -5,9 +5,21 @@
         </h2>
     </x-slot>
     <h1>Lista de Productos</h1>
-    <ul>
-        @foreach($products as $product)
-            <li>{{ $product->name }}</li>
-        @endforeach
-    </ul>
+    <div class="container mx-auto px-4">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
+            @foreach($products as $product)
+                <div class="bg-white shadow-md rounded-lg overflow-hidden">
+                    <img src="https://via.placeholder.com/150" class="h-10" alt="{{ $product->name }}">
+                    <div class="p-4">
+                        <h5 class="text-lg font-semibold">{{ $product->name }}</h5>
+                        <p class="text-gray-600">{{ $product->description }}</p>
+                        <p class="text-gray-800 font-bold mt-2">Price: ${{ $product->price }}</p>
+                        <p class="text-gray-600">Stock: {{ $product->stock }}</p>
+                        <p class="text-gray-600">Status: {{ $product->status }}</p>
+                        <p class="text-gray-600">SKU: {{ $product->sku }}</p>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 </x-app-layout>
